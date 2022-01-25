c769e43a18623ded13c58a7f719a66ae8a2aef47
Egorgr2 <91964708+Egorgr2@users.noreply.github.com>
2022-01-25 19:09:35 +0200

    Add files via upload

---
+++ lesson5.js
@@ -0,0 +1,46 @@
+for(let num = 1; num <= 10; num += 1){
+  console.log(num)
+}
+
+
+
+
+
+
+
+
+let i = 0;
+while (i < 11) {
+  if (i % 2 == 0) {
+    console.log(i, " парне");
+  } else {
+    console.log(i, " непарне");
+  }
+  i = i + 1;
+}
+
+
+
+
+
+let counter = 20;
+
+while (counter < 28){
+  counter += 1
+}
+
+console.log(counter)
+
+
+
+
+
+let name = '';
+do {
+   name= prompt('Введіть імя вашого улюбленого героя');
+} while (name.length > 6);
+console.log(name);
+
+
+
+
